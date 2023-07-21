dc2ab35fd756f5f2c1e2859194ce806686982dc3
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2023-07-21 15:53:25 +0530

    Add volume controls

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
 #include <raylib.h>
+#include <stdio.h>
 
-#define BG (Color){ 29, 32, 33, 1 }
+#define BG                                                                     \
+  (Color) { 29, 32, 33, 1 }
 
 int main() {
   InitWindow(800, 600, "Music Visualizer");
@@ -11,10 +12,13 @@ int main() {
   Music music = LoadMusicStream("./audio/7th Floor Tango - Silent Partner.mp3");
   PlayMusicStream(music);
 
+  float volume = 0.8f;
+
   while (!WindowShouldClose()) {
 
+    SetMusicVolume(music, volume);
     UpdateMusicStream(music);
-    
+
     if (IsKeyPressed(KEY_SPACE)) {
       if (IsMusicStreamPlaying(music)) {
         PauseMusicStream(music);
@@ -23,7 +27,15 @@ int main() {
       }
     }
 
+    if (IsKeyPressed(KEY_VOLUME_UP) || IsKeyPressed(KEY_UP)) {
+      if (volume < 1.0f)
+        volume += 0.1f;
+    }
 
+    if (IsKeyPressed(KEY_VOLUME_DOWN) || IsKeyPressed(KEY_DOWN)) {
+      if (volume > 0.0f)
+        volume -= 0.1f;
+    }
 
     BeginDrawing();
     ClearBackground(BG);
