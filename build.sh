6e02cb2215d67cc5dacc54b6a428c887a180ce4c
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2023-07-21 15:33:17 +0530

    Make music stream play
    * Added raylib to the project
    * Created a window with custom BG color
    * Play music inside an event loop

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -xe
 
-CFLAGS="-Wall -Wextra"
+CFLAGS="-Wall -Wextra `pkg-config --cflags raylib`"
+LIBS="`pkg-config --libs raylib` -lm -ldl -lpthread"
 
-clang $CFLAGS -o ./bin/visualizer main.c
+clang $CFLAGS -o ./bin/visualizer main.c $LIBS
--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <raylib.h>
+
+#define BG (Color){ 29, 32, 33, 1 }
 
 int main() {
-  printf("Hello, World!\n");
+  InitWindow(800, 600, "Music Visualizer");
+  SetTargetFPS(60);
+
+  InitAudioDevice();
+  Music music = LoadMusicStream("./audio/7th Floor Tango - Silent Partner.mp3");
+  PlayMusicStream(music);
+
+  int PLAY=1;
+
+  while (!WindowShouldClose()) {
+
+    UpdateMusicStream(music);
+
+    BeginDrawing();
+    ClearBackground(BG);
+    EndDrawing();
+  }
+
   return 0;
 }
