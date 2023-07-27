081734815c7576e2c87eedd31ed3c896295f0e44
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2023-07-27 21:33:16 +0530

    Draw audio samples to the screen
    * Audio samples will be displayed in the form of rectangles
    * Split the screen into 2 and draw an upward rectangle for positive
      values and downward for negative values
    * Uses a global frame and frame count variable to keep track of the
      sample value each frame

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <raylib.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-#define BG                                                                     \
-  (Color) { 29, 32, 33, 1 }
+#define BG (Color) { 29, 32, 33, 1 }
+
+#define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+
+uint32_t global_frames[1024] = {0};
+size_t global_frames_count = 0;
+
+void callback(void *bufferData, unsigned int frames) {
+  if (frames > ARRAY_LEN(global_frames)/2) {
+    frames = ARRAY_LEN(global_frames)/2;
+  }
+  memcpy(global_frames, bufferData, sizeof(uint32_t)*frames*2);
+  global_frames_count = frames*2;
+}
 
 int main() {
   InitWindow(800, 600, "Music Visualizer");
@@ -10,9 +26,16 @@ int main() {
 
   InitAudioDevice();
   Music music = LoadMusicStream("./audio/7th Floor Tango - Silent Partner.mp3");
+  printf("music.frameCount = %u\n", music.frameCount);
+  printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
+  printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
+  printf("music.stream.channels = %u\n", music.stream.channels);
+
   PlayMusicStream(music);
 
   float volume = 0.8f;
+
+  AttachAudioStreamProcessor(music.stream, callback);
 
   while (!WindowShouldClose()) {
 
@@ -37,8 +60,23 @@ int main() {
         volume -= 0.1f;
     }
 
+    int w = GetRenderWidth();
+    int h = GetRenderHeight();
+
     BeginDrawing();
     ClearBackground(BG);
+    float cell_width = (float)w/global_frames_count;
+    for (size_t i = 0; i < global_frames_count; i++) {
+      int16_t sample = *(int16_t*)&global_frames[i];
+      if (sample > 0) {
+        float t = (float)sample/INT16_MAX;
+        DrawRectangle(i*cell_width, h/2 - h/2*t, cell_width, h/2*t, RED);
+      } else {
+        float t = (float)sample/INT16_MIN;
+        DrawRectangle(i*cell_width, h/2, cell_width, h/2*t, RED);
+      }
+    }
+    // if (global_frames_count > 0) exit(0);
     EndDrawing();
   }
 
